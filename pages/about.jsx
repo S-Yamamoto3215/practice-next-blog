d56ceb8a7ca41dbd5c6cd714434c45aaf95bb420
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2022-08-12 12:08:44 +0900

    Setting meta data pageDesc

--- pages/about.jsx
+++ pages/about.jsx
@@ -10,7 +10,10 @@ import eyecatch from "images/about.jpg";
 export default function About() {
   return (
     <Container>
-      <Meta pageTitle="アバウト" />
+      <Meta
+        pageTitle="アバウト"
+        pageDesc="About development activities"
+      />
 
       <Hero
         title="About"
