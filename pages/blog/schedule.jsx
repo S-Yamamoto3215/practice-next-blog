f3333cdba71a9ccaf7c57133819e7dbcdd461521
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2022-08-15 10:50:50 +0900

    render Post eyecatch

--- pages/blog/schedule.jsx
+++ pages/blog/schedule.jsx
@@ -1,6 +1,7 @@
 import { getPostBySlug } from "lib/api";
 import Container from "components/container";
 import PostHeader from "components/post-header";
+import Image from "next/image";
 
 export default function Schedule({
   title,
@@ -16,6 +17,18 @@ export default function Schedule({
         subtitle='Blog Article'
         publish={publish}
       />
+
+      <figure>
+        <Image
+          src={eyecatch.url}
+          alt=""
+          layout='responsive'
+          width={eyecatch.width}
+          height={eyecatch.height}
+          sizes="(min-width: 1152px) 1152px, 100vw"
+          priority
+        />
+      </figure>
     </Container>
   )
 }
