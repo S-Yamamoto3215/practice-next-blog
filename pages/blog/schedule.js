2af620fd9211881a6d09116fd1687248b15dfa24
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2022-08-15 09:24:11 +0900

    microCMS Client Get Func await

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -8,7 +8,13 @@ export async function getStaticProps() {
   const resPromise = client.get({
     endpoint: 'blogs',
   });
-  console.log(resPromise);
+
+  try {
+    const res = await resPromise;
+    console.log(res);
+  } catch (err) {
+    console.log(err);
+  }
 
   return {
     props: {},
